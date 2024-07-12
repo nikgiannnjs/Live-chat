22ad2a6c2ed5b315fcbc6df7306d56d15d62281c
nikos <nikgiannsm@gmail.com>
2024-07-12 15:02:43 +0300

    Added http and socket servers, and emit function

--- app.js
+++ app.js
@@ -7,9 +7,8 @@ app.use(express.urlencoded({ extended: false }));
 
 app.get("/messages", async (req, res) => {
   try {
-    await Message.find({}, (err, messages) => {
-      res.send(messages);
-    });
+    const message = await Message.find();
+    res.status(200).json(message);
   } catch {
     res.status(500).json({
       status: "get /messages",
@@ -23,6 +22,7 @@ app.post("/messages", async (req, res) => {
     const message = new Message(req.body);
     await message.save((err) => {
       if (err) sendStatus(500);
+      io.emit("message", req.body);
       res.sendStatus(200);
     });
   } catch {
